340b0770c702857db483220c3c2fa9bfa0acfe55
divyansh18123 <divyansh24201@iiitd.ac.in>
2025-02-16 21:10:43 +0530

    Add files via upload

---
+++ CO_Project_I_S_Instructions.py
@@ -0,0 +1,2 @@
+i_dict = {"1w" : ["010","0000011"], "addi" : ["000", "0010011"], "jalr" : ["000", "1100111"]} # instruction : { funct3 , opcode}
+s_dict = {"sw" : ["010" , "0100011"]} # instruction : { funct3 , opcode}
